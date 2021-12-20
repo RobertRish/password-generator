1bd24734f54cbbf14492a6200edf6a68d3ef6d58
Robby Rish <robbyrish13@gmail.com>
2021-12-19 21:21:37 -0500

    synced number slider with text box

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,61 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Everything I've added:
+
+//Syncs "Number of Characters slider with text input"
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeLowercaseElement = document.getElementById('includeLowercase')
+const includeNumericElement = document.getElementById('includeNumeric')
+const includeSpecialElement = document.getElementById('includeSpecial')
+const UppercaseCharCodes = arrayFromLowToHigh(65, 90)
+const LowercaseCharCodes = arrayFromLowToHigh(97, 122)
+const NumericCharCodes = arrayFromLowToHigh(48, 57)
+const SpecialCharCodes = arrayFromLowToHigh(33, 47).concat(58, 64).concat(91, 96).concat(123, 126)
+
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
+form.addEventListener('submit', e => {e.preventDefault
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercase.checked
+  const includeLowercase = includeLowercase.checked
+  const includeNumeric = includeNumeric.checked
+  const includeSpecial = includeSpecial.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeLowercase, includeNumeric, includeSpecial)
+})
+
+function generatePassword(characterAmount, includeUppercase, includeNumeric, includeSpecial) {
+  let charCodes = LowercaseCharCodes
+  if (includeUppercase) charCodes = charCodes.concat(UppercaseCharCodes)
+  if (includeNumeric) charCodes = charCodes.concat(NumericCharCodes)
+  if (includeSpecial) charCodes = charCodes.concat(SpecialCharCodes)
+
+  const passwordCharacters = []
+  for (let i = 0, i < characterAmount, i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i) 
+  }
+  return array
+}
+
+//if I need to stop password generator button from refreshing page: 16
+
